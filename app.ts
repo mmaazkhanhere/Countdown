a7070541a1ac652fbc1a97cbf35f9a4b9e8f0880
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-01-27 12:32:01 +0500

    Add files via upload

--- app.ts
+++ app.ts
@@ -3,68 +3,78 @@ import inquirer from "inquirer";
 import chalk from "chalk";
 import chalkAnimation from "chalk-animation";
 
-let hours:number=0;
+let hours:number=0; //initialising hours, minutes, and seconds variables
 let minutes:number=0;
 let seconds:number=0;
 
- async function setTime(){
+ async function setTime(){ //function to get user input
+
     var getTime=await inquirer.prompt([{
-        name:'getHour',
+        name:'getHour', //getting hour 
         type:'input',
         message:'Enter hours (24 hour format) '
     },
     {
-        name:'getMinute',
+        name:'getMinute', //getting minute
         type:'input',
         message:'Enter minutes: '
     },
     {
-        name:'getSeconds',
+        name:'getSeconds', //getting seconds
         type:'input',
         message:'Enter seconds: '
     }
 ]);
-    hours=getTime.getHour;
+    hours=getTime.getHour; //change the values of hours, minutes and seconds depending on user input
     minutes=getTime.getMinute;
     seconds=getTime.getSeconds;
-}
 
-async function countDown(){
-    console.clear();
-    console.log(`${hours} : ${minutes} : ${seconds}`);
-    seconds -= 1;
-    if((hours <24 || hours >= 0) && (minutes <60 || minutes >= 0) && (seconds <60 || seconds > 0)){
-        if(seconds == 0){
-            if(minutes==0){
-                if (hours > 0){
-                    minutes +=59;
-                    seconds += 60
-                    hours -=1;
-                    countDown();
-                }
-                else{
-                    countDown();
-                }
-            }
-            else{
-                seconds +=60;
-                minutes -= 1;
-                countDown();
-            }
-        }
-        else if(seconds>0){
-            setTimeout(()=>{
-                countDown();
-            },0.1)
-        }
-        else if(seconds<=0){
-            console.log(chalk.bgRed('Alarm'))
-        }
+    if((hours>=0 && hours<24) && (minutes>=0 && minutes<60) && (seconds>=0 && seconds<60)){
+        //if user input is in countdown format, the countdown function will be called
+        await countDown(); //calling the countdown function
     }
-    else{
+    else{ //if user input is not in specified format, the following error message will be displayed
         console.log(`Wrong input. Please enter value within range.`)
     }
 }
 
-await setTime();
-countDown();    
+async function countDown(){
+    //function for countdown process
+        console.clear(); //clear the console everytime the function restart
+        console.log(`${hours} : ${minutes} : ${seconds}`); //time will be displayed
+        seconds -= 1; //decrement second value
+        if(seconds == 0){
+            //if second is equal to zero, check whether minute is equal to zero
+            if(minutes==0){ 
+                //if minute is equal to zero, check whether hour is equal to zero
+                if (hours > 0){
+                    //if hour is greater than zero, decrement one from hour and restart minute and seconds
+                    minutes +=59;
+                    seconds += 60
+                    hours -=1;
+                    countDown(); //calling the function again
+                }
+                else{
+                    //else ig hour is zero go back to the function start again
+                    countDown();
+                }
+            }
+            else{
+                //if minute is left, decrement one from it and restart seconds
+                seconds +=60;
+                minutes -= 1;
+                countDown(); //calling the function again
+            }
+        }
+        else if(seconds>0){
+            //if seconds are left, decrement it
+            setTimeout(()=>{
+                countDown();
+            },1000)
+        }
+        else if(seconds<0){
+            //if no seconds are left, display alarm on the screen
+            console.log(chalk.bgRed('Alarm'))
+        }
+}
+setTime();
